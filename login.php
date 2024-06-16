e522080edf3a8f5b6225f0697825faa0812ca02d
JLock334 <Lockhartjaquarious@gmail.com>
2024-06-16 18:29:16 -0400

    added debugging

--- login.php
+++ login.php
@@ -1,4 +1,8 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
